2343df9908695e036eea4ce900fbf9b9ee9b20bc
Khayyal AbaAlkhayl <khayyalabd@gmail.com>
2026-02-18 04:25:39 +0300

    Advance JS

---
+++ Advanced/DOMintro/DOM.js
@@ -0,0 +1,67 @@
+/*
+  DOM
+  - What Is DOM
+  - DOM Selectors
+  --- Find Element By ID
+  --- Find Element By Tag Name
+  --- Find Element By Class Name
+  --- Find Element By CSS Selectors
+  --- Find Element By Collection
+  ------ title
+  ------ body
+  ------ images
+  ------ forms
+  ------ links
+*/
+
+// let myIdElement = document.getElementById("my-div");
+// let myTagElements = document.getElementsByTagName("p");
+// let myClassElement = document.getElementsByClassName("my-span");
+// let myQueryElement = document.querySelector(".my-span");
+// let myQueryAllElement = document.querySelectorAll(".my-span");
+
+// console.log(myIdElement);
+// console.log(myTagElements[1]);
+// console.log(myClassElement[1]);
+// console.log(myQueryElement);
+// console.log(myQueryAllElement[1]);
+
+// console.log(document.title);
+// console.log(document.body);
+// console.log(document.forms[0].one.value);
+// console.log(document.links[0].href);
+
+/*
+  DOM [Get / Set Elements Content And Attributes]
+  - innerHTML
+  - textContent
+  - Change Attributes Directly
+  - Change Attributes With Methods
+  --- getAttribute
+  --- setAttribute
+
+  Search
+  - innerText
+*/
+
+let myElement = document.querySelector(".js");
+
+console.log(myElement.innerHTML);
+console.log(myElement.textContent);
+
+myElement.innerHTML = "Text From <span>Main.js</span> File";
+myElement.textContent = "Text From <span>Main.js</span> File";
+
+document.images[0].src = "https://google.com";
+document.images[0].alt = "Alternate";
+document.images[0].title = "Picture";
+document.images[0].id = "pic";
+document.images[0].className = "img";
+
+let myLink = document.querySelector(".link");
+
+console.log(myLink.getAttribute("class"));
+console.log(myLink.getAttribute("href"));
+
+myLink.setAttribute("href", "https://twitter.com");
+myLink.setAttribute("title", "Twitter");
---
+++ basics/lesson2.js
@@ -0,0 +1,421 @@
+/*
+  Higher Order Functions
+  ---> is a function that accepts functions as parameters and/or returns a function.
+
+  - Map
+  --- method creates a new array
+  --- populated with the results of calling a provided function on every element
+  --- in the calling array.
+
+  Syntax map(callBackFunction(Element, Index, Array) { }, thisArg)
+  - Element => The current element being processed in the array.
+  - Index => The index of the current element being processed in the array.
+  - Array => The Current Array
+
+  Notes
+  - Map Return A New Array
+
+  Examples
+  - Anonymous Function
+  - Named Function
+
+*/
+
+// let myNums = [1, 2, 3, 4, 5, 6];
+
+// let newArray = [];
+
+// for (let i = 0; i < myNums.length; i++) {
+//   newArray.push(myNums[i] + myNums[i]);
+// }
+
+// console.log(newArray);
+
+// // Same Idea With Map
+
+// let addSelf = myNums.map(function (element, index, arr) {
+// //   console.log(`Current Element => ${element}`);
+// //   console.log(`Current Index => ${index}`);
+// //   console.log(`Array => ${arr}`);
+// //   console.log(`This => ${this}`);
+// return element + element;
+// }, 10);
+
+// // let addSelf = myNums.map((a) => a + a);
+
+// console.log(addSelf);
+
+// // function addition(ele) {
+// //   return ele + ele;
+// // }
+
+// // let add = myNums.map(addition);
+
+// // console.log(add);
+
+// /*
+//   Map
+//   - Swap Cases
+//   - Inverted Numbers
+//   - Ignore Boolean Value
+// */
+
+// let swappingCases = "elZERo";
+// let invertedNumbers = [1, -10, -20, 15, 100, -30];
+// let ignoreNumbers = "Elz123er4o";
+
+// // let sw = swappingCases
+// //   .split("")
+// //   .map(function (ele) {
+// //     // Condition ? True : False
+// //     return ele === ele.toUpperCase() ? ele.toLowerCase() : ele.toUpperCase();
+// //   })
+// //   .join("");
+
+// // Arrow Function Version
+
+// let sw = swappingCases
+//   .split("")
+//   .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
+//   .join("");
+
+// console.log(sw);
+
+// let inv = invertedNumbers.map(function (ele) {
+//   return -ele;
+// });
+
+// console.log(inv);
+
+// let ign = ignoreNumbers
+//   .split("")
+//   .map(function (ele) {
+//     return isNaN(parseInt(ele)) ? ele : "";
+//   })
+//   .join("");
+
+// console.log(ign);
+
+/*
+  - Filter
+  --- method creates a new array
+  --- with all elements that pass the test implemented by the provided function.
+
+  Syntax filter(callBackFunction(Element, Index, Array) { }, thisArg)
+  - Element => The current element being processed in the array.
+  - Index => The index of the current element being processed in the array.
+  - Array => The Current Array
+*/
+
+// // Get Friends With Name Starts With A
+// let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
+
+// let filterdFriends = friends.filter(function (el) {
+//   return el.startsWith("A") ? true : false;
+// });
+
+// console.log(filterdFriends);
+
+// // Get Even Numbers Only
+// let numbers = [11, 20, 2, 5, 17, 10];
+
+// let evenNumbers = numbers.filter(function (el) {
+//   return el % 2 === 0;
+// });
+
+// console.log(evenNumbers);
+
+// // Test Map vs Filter
+
+// // let addMap = numbers.map(function (el) {
+// //   return el + el;
+// // });
+
+// // console.log(addMap);
+
+// // let addFilter = numbers.filter(function (el) {
+// //   return el + el;
+// // });
+
+// // console.log(addFilter);
+
+/*
+  - Reduce
+  --- method executes a reducer function on each element of the array,
+  --- resulting in a single output value.
+
+  Syntax
+  reduce(callBackFunc(Accumulator, Current Val, Current Index, Source Array) { }, initialValue)
+  - Accumulator => the accumulated value previously returned in the last invocation
+  - Current Val => The current element being processed in the array
+  - Index => The index of the current element being processed in the array.
+  ---------- Starts from index 0 if an initialValue is provided.
+  ---------- Otherwise, it starts from index 1.
+  - Array => The Current Array
+*/
+
+// let nums = [10, 20, 15, 30];
+
+// let add = nums.reduce(function (acc, current, index, arr) {
+//   console.log(`Acc => ${acc}`);
+//   console.log(`Current Element => ${current}`);
+//   console.log(`Current Element Index => ${index}`);
+//   console.log(`Array => ${arr}`);
+//   console.log(acc + current);
+//   console.log(`#############`);
+//   return acc + current;
+// },1);
+
+// console.log(add);
+
+// let a
+
+/*
+  - forEach
+  --- method executes a provided function once for each array element.
+
+  Syntax forEach(callBackFunction(Element, Index, Array) { }, thisArg)
+  - Element => The current element being processed in the array.
+  - Index => The index of the current element being processed in the array.
+  - Array - The Current Array
+
+  Note
+  - Doesnt Return Anything [Undefined]
+  - Break Will Not Break The Loop
+*/
+
+// let allLis = document.querySelectorAll("ul li");
+
+// let allDivs = document.querySelectorAll(".content div");
+
+// allLis.forEach(function (ele) {
+//   ele.onclick = function () {
+//     // Remove Active Class From All Elements
+//     allLis.forEach(function (ele) {
+//       ele.classList.remove("active");
+//     });
+//     // Add Active Class To This Element
+//     this.classList.add("active");
+//     // Hide All Divs
+//     allDivs.forEach(function (ele) {
+//       ele.style.display = "none";
+//     });
+//   };
+// });
+
+/*
+  Object
+  - Intro and What Is Object
+  - Testing Window Object
+  - Accessing Object
+*/
+
+// let user = {
+//   // Properties
+//   theName: "Osama",
+//   theAge: 38,
+//   // Methods
+//   sayHello: function () {
+//     return `Hello`;
+//   },
+// };
+
+// console.log(user.theName);
+// console.log(user.theAge);
+// console.log(user.sayHello());
+
+/*
+  Object
+  - Dig Deeper
+  - Dot Notation vs Bracket Notation
+  - Dynamic Property Name
+*/
+
+// let myVar = "country";
+
+// let user = {
+// theName: "Osama",
+// country: "Egypt",
+// };
+
+// console.log(user.theName);
+// console.log(user.country); // user.country
+// console.log(user.myVar); // user.country
+// console.log(user[myVar]); // user.country
+
+/*
+  Object
+  - Nested Object And Trainings
+*/
+
+// let available = true;
+
+// let user = {
+//   name: "Osama",
+//   age: 38,
+//   skills: ["HTML", "CSS", "JS"],
+//   available: false,
+//   addresses: {
+//     ksa: "Riyadh",
+//     egypt: {
+//       one: "Cairo",
+//       two: "Giza",
+//     },
+//   },
+//   checkAv: function () {
+//     if (user.available === true) {
+//       return `Free For Work`;
+//     } else {
+//       return `Not Free`;
+//     }
+//   },
+// };
+
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.skills);
+// console.log(user.skills.join(" | "));
+// console.log(user.skills[2]); // Access With Index
+// console.log(user.addresses.ksa);
+// console.log(user.addresses.egypt.one);
+// console.log(user["addresses"].egypt.one);
+// console.log(user["addresses"]["egypt"]);
+// console.log(user["addresses"]["egypt"]["one"]);
+
+// console.log(user.checkAv());
+
+// /*
+//   Object
+//    Create With New Keyword new Object();
+
+// let user = new Object({
+//   age: 20,
+// });
+
+// console.log(user);
+
+// user.age = 38;
+// user["country"] = "Egypt";
+
+// user.sayHello = function () {
+//   return `Hello`;
+// };
+
+// console.log(user);
+// console.log(user.age);
+// console.log(user.country);
+// console.log(user.sayHello());
+
+/*
+  Function this Keyword
+  - this Introduction
+  - this Inside Object Method
+  --- When a function is called as a method of an object,
+  --- its this is set to the object the method is called on.
+  - Global Object
+  - Test Variables With Window And This
+  - Global Context
+  - Function Context
+
+  Search
+  - Strict Mode
+*/
+
+// console.log(this);
+
+// console.log(window);
+
+
+// myVar = 100;
+
+// console.log(this.myVar);
+// console.log(this.myVar);
+
+// function sayHello() {
+//   console.log(this);
+//   return this;
+// }
+// sayHello();
+
+// console.log(sayHello() === window);
+
+// document.getElementById("cl").onclick = function () {
+//   console.log(this);
+// };
+
+// let user = {
+//   age: 38,
+//   ageInDays: function () {
+//     console.log(this);
+//     return this.age * 365;
+//   },
+// };
+
+// console.log(user.age);
+// console.log(user.ageInDays());
+
+
+/*
+  Object
+  - Create Object With Create Method
+*/
+
+// let user = {
+//   age: 20,
+//   doubleAge: function () {
+//     return this.age * 2;
+//   },
+// };
+
+// console.log(user);
+// console.log(user.age);
+// console.log(user.doubleAge());
+
+// let obj = Object.create({});
+
+// obj.a = 100;
+
+// console.log(obj);
+
+// let copyObj = Object.create(user);
+
+// copyObj.age = 50;
+
+// console.log(copyObj);
+// console.log(copyObj.age);
+// console.log(copyObj.doubleAge());
+
+/*
+  Object
+  - Create Object With Assign Method
+*/
+
+let obj1 = {
+  prop1: 1,
+  meth1: function () {
+    return this.prop1;
+  },
+};
+
+let obj2 = {
+  prop2: 2,
+  meth2: function () {
+    return this.prop2;
+  },
+};
+
+let targetObject = {
+  prop1: 100,
+  prop3: 3,
+};
+
+let finalObject = Object.assign(targetObject, obj1, obj2);
+
+finalObject.prop1 = 200;
+finalObject.prop4 = 4;
+
+console.log(finalObject);
+
+let newObject = Object.assign({}, obj1, { prop5: 5, prop6: 6 });
+
+console.log(newObject);
